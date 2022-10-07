300d87472e8cd6e10834b27838981b1dc5e0148b
vcss7 <vs.victorsolis@gmail.com>
2022-10-07 16:38:51 -0700

    luhns_algorithm init

---
+++ Luhns-Algorithm/luhns_algorithm.c
@@ -0,0 +1,26 @@
+/*
+ * File:    luhn_algorithm.c
+ *
+ * Purpose: Determine whether a user provided number is a valid credit card
+ *      number.
+ *
+ * Compile: gcc -g -Wall -o luhn_algorithm.a luhn_algorithm.c
+ * Run:     ./luhn_algorithm.a      # from inside the directory
+ *
+ * Input:   Number(s)
+ *
+ * Output:  A list of the numbers provided and the result
+ *
+ * IPP:     Operating System's Project 1
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
+
