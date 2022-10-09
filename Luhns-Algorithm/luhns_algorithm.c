f3b9e5035ba086c172b3af949dd3196a1af4f149
vcss7 <vs.victorsolis@gmail.com>
2022-10-09 01:25:03 -0700

    luhns_algorithm: read from list_of_numbers file

--- Luhns-Algorithm/luhns_algorithm.c
+++ Luhns-Algorithm/luhns_algorithm.c
@@ -15,12 +15,41 @@
  */
 
 #include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+const uint8_t NUMBER_LIST_LENGTH = 10;
 
 int main (int argc, char *argv[])
 {
+    char filename[] = "list_of_numbers.txt";
+    FILE *file_ptr = fopen(filename, "r+e");
+    unsigned int numbers[NUMBER_LIST_LENGTH];
+
+    if(file_ptr == NULL)
+    {
+        printf("Could not open file: %s", filename);
+        exit(1);
+    }
+
+    /* Read number list from file */
+    #pragma unroll
+    for (int i = 0; i < NUMBER_LIST_LENGTH; i++)
+    {
+        fscanf(file_ptr, "%u", &numbers[i]);
+    }
+
+    fclose(file_ptr);
+
+    /* Print number list */
+    #pragma unroll
+    for (int i = 0; i < NUMBER_LIST_LENGTH; i++)
+    {
+        printf("Number[%d] = %u\n", i, numbers[i]);
+    }
+
     return 0;
 }
 
