6dad638d347e859a431ad2e678a932c6a1f500c7
vcss7 <vs.victorsolis@gmail.com>
2022-10-09 16:08:59 -0700

    luhns_algorithm: add credit card struct

--- Luhns-Algorithm/luhns_algorithm.c
+++ Luhns-Algorithm/luhns_algorithm.c
@@ -2,7 +2,7 @@
  * File:    luhn_algorithm.c
  *
  * Purpose: Determine whether a user provided number is a valid credit card
- *      number.
+ *          number.
  *
  * Compile: gcc -g -Wall -o luhn_algorithm.a luhn_algorithm.c
  * Run:     ./luhn_algorithm.a      # from inside the directory
@@ -14,6 +14,7 @@
  * IPP:     Operating System's Project 1
  */
 
+#include <inttypes.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -22,11 +23,71 @@
 
 const uint8_t NUMBER_LIST_LENGTH = 10;
 
+struct CreditCardIssuer
+{
+    char* name;
+    uint8_t id;
+};
+
+struct CreditCard
+{
+    uint64_t number;
+    uint8_t length;
+    struct CreditCardIssuer issuer;
+    bool valid;
+};
+
+
+void get_num_from_file(struct CreditCard* credit_card_p);
+void print_credit_card_details(struct CreditCard* credit_card_p);
+
+
 int main (int argc, char *argv[])
+{
+    /* allocate memory for numbers */
+    struct CreditCard* credit_card = NULL;
+    credit_card = malloc(NUMBER_LIST_LENGTH * sizeof(struct CreditCard));
+
+    /* program */
+    get_num_from_file(credit_card);
+    print_credit_card_details(credit_card);
+
+    /* free allocated memory */
+    free(credit_card);
+
+    return 0;
+}
+
+
+void print_credit_card_details(struct CreditCard* credit_card_p)
+{
+    for (int i = 0; i < NUMBER_LIST_LENGTH; i++)
+    {
+        if (credit_card_p[i].valid)
+        {
+            printf("%8s", "valid");
+            printf(" | ");
+            printf("%20"PRIu64"", credit_card_p[i].number);
+            printf("%2u", credit_card_p[i].length);
+            printf("%2u", credit_card_p[i].issuer.id);
+            printf("%s", credit_card_p[i].issuer.name);
+            printf("\n");
+        }
+        else
+        {
+            printf("%8s", "invalid");
+            printf(" | ");
+            printf("%"PRIu64"", credit_card_p[i].number);
+            printf("\n");
+        }
+    }
+}
+
+
+void get_num_from_file(struct CreditCard* credit_card_p)
 {
     char filename[] = "list_of_numbers.txt";
     FILE *file_ptr = fopen(filename, "r+e");
-    unsigned int numbers[NUMBER_LIST_LENGTH];
 
     if(file_ptr == NULL)
     {
@@ -38,18 +99,13 @@ int main (int argc, char *argv[])
     #pragma unroll
     for (int i = 0; i < NUMBER_LIST_LENGTH; i++)
     {
-        fscanf(file_ptr, "%u", &numbers[i]);
+        fscanf(file_ptr, "%"PRIu64"", &credit_card_p[i].number);
+        credit_card_p[i].length = 0;
+        credit_card_p[i].issuer.id = 0;
+        credit_card_p[i].issuer.name = "NONE";
+        credit_card_p[i].valid = false;
     }
 
     fclose(file_ptr);
-
-    /* Print number list */
-    #pragma unroll
-    for (int i = 0; i < NUMBER_LIST_LENGTH; i++)
-    {
-        printf("Number[%d] = %u\n", i, numbers[i]);
-    }
-
-    return 0;
 }
 
